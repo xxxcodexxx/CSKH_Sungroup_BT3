10f0dd56bdd197970b48f8d8c31ced7705a78566
NguyenThanhDon <nhockimi96tn@gmail.com>
2018-02-21 10:13:54 +0700

    delete About, Contact Pages
    delete About and Contact Pages

--- CSKH_Sungroup_BT3/CSKH_Sungroup_BT3/Controllers/HomeController.cs
+++ CSKH_Sungroup_BT3/CSKH_Sungroup_BT3/Controllers/HomeController.cs
@@ -12,19 +12,5 @@ namespace CSKH_Sungroup_BT3.Controllers
         {
             return View();
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
